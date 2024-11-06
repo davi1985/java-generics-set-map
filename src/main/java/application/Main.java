f79bbdfb7f9cfddb3916470cc6f3e90f8ad941b1
Davi Silva <davisilvaphoto@gmail.com>
2024-11-06 13:51:54 -0300

    intro generics

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+
+import services.PrintService;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        PrintService<String> service = new PrintService();
+
+        System.out.print("How many values ? ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = scanner.next();
+            service.addValue(value);
+        }
+
+        service.print();
+
+        System.out.println("First: " + service.first());
+
+        scanner.close();
+    }
+}
