989c9c22c01bec57947a739081693908ad033949
Davi Silva <davisilvaphoto@gmail.com>
2024-11-12 08:23:59 -0300

    limited generics - final implementation

--- src/main/java/application/Main2.java
+++ src/main/java/application/Main2.java
@@ -1,5 +1,6 @@
 package application;
 
+import entities.Product;
 import services.CalculationService;
 
 import java.io.BufferedReader;
@@ -7,23 +8,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Main2 {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
         String path = "in.txt";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line = reader.readLine();
 
             while (line != null) {
-                list.add(Integer.parseInt(line));
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = reader.readLine();
             }
 
-            Integer result = CalculationService.max(list);
-            System.out.println("Max: " + result);
+            Product result = CalculationService.max(list);
+            System.out.println("Most expansive: " + result);
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Product {
+public class Product implements Comparable<Product> {
 
     private String name;
     private Double price;
@@ -32,5 +32,10 @@ public class Product {
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
+    }
+
+    @Override
+    public int compareTo(Product obj) {
+        return price.compareTo(obj.getPrice());
     }
 }
